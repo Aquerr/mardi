dd2caf6c26eb0977faa199d08fdaa01657ba1f6e
Mateusz <mtpala@interia.pl>
2024-02-07 09:08:28 +0100

    Init commit

---
+++ src/main/java/com/mateo9x/mardi/MardiApplication.java
@@ -0,0 +1,13 @@
+package com.mateo9x.mardi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MardiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MardiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mateo9x/mardi/MardiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateo9x.mardi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MardiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
